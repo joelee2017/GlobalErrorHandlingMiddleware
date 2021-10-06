579626a7211663d0eec7c9ba1410dc330397315e
joe <as955133@gmail.com>
2021-10-06 21:18:36 +0800

    底版建立

---
+++ MvcMovie/MvcMovie/Helper/InJectionExtension.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.Extensions.DependencyInjection;
+using Model.Models;
+using Model.Repository;
+using Service.Service;
+
+namespace MvcMovie.Helper
+{
+    public static class InJectionExtension
+    {
+        public static void InJectionByRepository(this IServiceCollection services)
+        {
+            services.AddScoped<IRepository<Movie>, MovieRepository>();
+        }
+
+        public static void InJectionByService(this IServiceCollection services)
+        {
+            services.AddScoped<IMoviesService, MoviesService>();
+        }
+    }
+}
